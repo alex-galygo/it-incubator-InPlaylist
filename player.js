fb8b6dc01b8064f55b419b3c162cce9e4fcabc07
Alex Galygo <alex.galygo.94@gmail.com>
2023-12-11 21:54:46 +0300

    Upgrade playlist structure in player.js and delete unused index.js
    Removed unused index.js file and overhauled playlist structure in player.js to include more details such as cover images for each song. Additionally, added two playlists to enhance UI and UX. Changes on index.html are necessary for correct display and scalability for different devices.

--- player.js
+++ player.js
@@ -3,32 +3,76 @@
 // Data
 let title = 'My Playlists';
 
-const playlist = {
-    id: 1,
+const playlist1 = {
     type: 'Playlist',
+    playlist_crc: 'assets/images/playlist-1-logo.png',
     name: 'Hip-Hop Hits',
     count_songs: 4,
     duration_all: 12.13,
     authors: ['Eminem', '50cent', 'Lloyd Banks'],
     songs: [
         {
-            id: 1,
-            cover_image: 'Image20231205223615.png',
+            cover_image: 'assets/images/eminem.png',
             name: 'Eminem',
             author: 'Rap God',
-            duration: 3.41,
             src: 'https://tonuz.net/uploads/music/2023/10/Eminem_Rap_God.mp3'
         },
         {
-            id: 2,
-            cover_image: 'Image20231205223549.png',
+            cover_image: 'assets/images/50-cent.png',
             name: 'In da Club',
             author: '50 cent',
-            duration: 4.31,
             src: 'https://muzfan.net/uploads/files/2020-07/1594719752_50-cent-in-da-club.mp3'
+        },
+        {
+            cover_image: 'assets/images/dmx.png',
+            name: 'X Gon\' Give It To Ya',
+            author: 'DMX',
+            src: 'https://cdn.muzonchek.ru/files/music/3026/DMX-feat-The-xx_-_XX-Gon-Give-It-To-Ya.mp3'
+        },
+        {
+            cover_image: 'assets/images/lyud.png',
+            name: 'You Don\'t Know',
+            author: 'Eminem feat 50 Cent, Cashis, Lloyd Banks',
+            src: 'https://cdn.muzonchek.ru/files/music/67784/Eminem-50-Cent-Cashis-Lloyd-Banks_-_You-Dont-Know.mp3'
         },
     ]
 }
+
+const playlist2 = {
+    type: 'Playlist',
+    playlist_crc: 'assets/images/playlist-2-logo.png',
+    name: 'Rap Hits 1990s',
+    count_songs: 4,
+    duration_all: 12.13,
+    authors: ['Public Enemy', 'Vanila Ice', 'MC Hammer'],
+    songs: [
+        {
+            cover_image: 'assets/images/public-enemy.png',
+            name: 'Fight the Power',
+            author: 'Public Enemy',
+            src: 'https://tonuz.net/uploads/music/2023/10/Eminem_Rap_God.mp3'
+        },
+        {
+            cover_image: 'assets/images/vanila-ice.png',
+            name: 'Ice Ice Baby',
+            author: 'Vanila Ice',
+            src: 'https://muzfan.net/uploads/files/2020-07/1594719752_50-cent-in-da-club.mp3'
+        },
+        {
+            cover_image: 'assets/images/Hammer.png',
+            name: 'You Can’t Touch This',
+            author: 'MC Hammer',
+            src: 'https://cdn.muzonchek.ru/files/music/3026/DMX-feat-The-xx_-_XX-Gon-Give-It-To-Ya.mp3'
+        },
+        {
+            cover_image: 'assets/images/brand-nubian.png',
+            name: 'Brand Nubian',
+            author: 'Brand Nubian',
+            src: 'https://cdn.muzonchek.ru/files/music/67784/Eminem-50-Cent-Cashis-Lloyd-Banks_-_You-Dont-Know.mp3'
+        },
+    ]
+}
+
 
 // Render
 
@@ -45,7 +89,7 @@ document.body.append(playlistElement);
 function renderPlaylistHeader(inputPlaylistForRendering) {
 
     let playlistImageElement = document.createElement('img');
-    playlistImageElement.src = 'Rectangle 101.png';
+    playlistImageElement.src = inputPlaylistForRendering.playlist_crc;
     playlistElement.append(playlistImageElement);
 
     let playlistTypeElement = document.createElement('h3');
@@ -99,7 +143,8 @@ function renderPlaylist(playlistForRendering) {
     renderTrack(playlistForRendering);
 }
 
-renderPlaylist(playlist);
+renderPlaylist(playlist1);
+renderPlaylist(playlist2);
 
 
 
